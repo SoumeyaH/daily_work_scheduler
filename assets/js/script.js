7d512039d7adb5836e74f51de9ccf3da20cf8707
Soumeya <soumeyahassan47@gmail.com>
2021-03-31 18:09:15 +0100

    paused for the day.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-// Wednesday, 31st of March, 13:35:12
-
 const getDateTimeAndAppend = () => {
   const pElement = $("#currentDay");
 
@@ -16,3 +14,19 @@ const secondsTicking = () => {
 };
 
 secondsTicking();
+
+// get the text area
+// link it to a hour
+// hour = get hour by id
+//get text by id
+// set text value as textarea input value
+// put both in object together
+// key hour, key text
+//make an array of objects - each hour
+
+//take that and put it in local storage
+
+// local storage
+// set item - pass in key and value, need to be string - JSON.stringify
+// get item JSON.parse
+// clear - for all, remove item - single key
